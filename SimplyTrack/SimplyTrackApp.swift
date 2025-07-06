e1d4c2d46dc6238d2f447f4855b19b23af8b18e7
Farhanhamim1111 <farhanhamim1111@gmail.com>
2025-07-05 23:32:32 -0400

    Initial Commit

---
+++ SimplyTrack/SimplyTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyTrackApp.swift
+//  SimplyTrack
+//
+//  Created by Farhan Hamim on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SimplyTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
