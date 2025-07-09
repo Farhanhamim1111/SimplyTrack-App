c6bf3afa6220fb356b3545250d6759fe069c0028
Farhanhamim1111 <farhanhamim1111@gmail.com>
2025-07-09 18:03:43 -0400

    finishing touches

--- SimplyTrack/ContentView.swift
+++ SimplyTrack/ContentView.swift
@@ -10,6 +10,49 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+        ZStack{
+            Image("background-cloth")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+            Image("button")
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                Spacer()
+            }
+        }
         
     }
 }
