61c11234562845b214afb9b3af669a5f4b0ed788
Farhanhamim1111 <farhanhamim1111@gmail.com>
2025-07-09 17:33:31 -0400

    restart

--- SimplyTrack/ContentView.swift
+++ SimplyTrack/ContentView.swift
@@ -10,56 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        ZStack{
-            Color(.systemMint)
-                .ignoresSafeArea()
-            
-            VStack(alignment: .leading, spacing: 20.0){
-                Image("Conquest")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(15)
-                
-                HStack{
-                    Text("Conquest")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    
-                    Spacer()
-                    VStack{
-                        HStack{
-                            Image(systemName: "star.fill")
-                            
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.leadinghalf.fill")
-                        }
-                        .foregroundColor(.orange)
-                        .font(.caption)
-                        
-                        Text("(Reviews)")
-                    }
-                    
-                }
-                
-                Text("Conquest")
-                
-                HStack{
-                    Spacer()
-                    Image(systemName: "binoculars.fill")
-                    Image(systemName: "fork.knife")
-                }
-                .foregroundColor(.gray)
-                .font(.caption)
-            }
-            .padding()
-            .background(Rectangle()
-                .foregroundStyle(.white)
-                .cornerRadius(15.0)
-                .shadow(radius: 15.0))
-            .padding()
-        }
+        
     }
 }
 
