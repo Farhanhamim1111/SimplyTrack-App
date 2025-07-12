0640f280fe0305276280d606c3778fc24117c94a
Farhanhamim1111 <farhanhamim1111@gmail.com>
2025-07-12 14:48:04 -0400

    background

--- SimplyTrack/ContentView.swift
+++ SimplyTrack/ContentView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
-    
     var body: some View {
         
-       
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.black, lineWidth: 3)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            padding(0)
+                
+
+        }
+        ignoresSafeArea()
         }
         
     }
