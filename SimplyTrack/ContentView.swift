533a748a025612b8b92b2e951051246616df0e68
Farhanhamim1111 <farhanhamim1111@gmail.com>
2025-07-12 14:38:48 -0400

    restart

--- SimplyTrack/ContentView.swift
+++ SimplyTrack/ContentView.swift
@@ -9,89 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var playerCard = "card13"
-    @State var cpuCard = "card7"
     
-    @State var playerScore = 0
-    @State var cpuScore = 0
     
     var body: some View {
         
-        ZStack{
-            Image("background-cloth")
-                .resizable()
-                .ignoresSafeArea()
-            VStack{
-                Spacer()
-                Image("logo")
-                Spacer()
-                HStack{
-                    Spacer()
-                    Image(playerCard)
-                    Spacer()
-                    Image(cpuCard)
-                    Spacer()
-                }
-                Spacer()
-            
-                Button {
-                    deal()
-                } label: {
-                    Image("button")
-                }
-
-                
-                Spacer()
-                HStack{
-                    Spacer()
-                    VStack{
-                        
-                        Text("Player")
-                            .font(.headline)
-                            .padding(.bottom, 10)
-                        Text(String(playerScore))
-                            .font(.largeTitle)
-                    }
-                    Spacer()
-                    
-                    VStack{
-                        Text("CPU")
-                            .font(.headline)
-                            .padding(.bottom, 10)
-                        Text(String(cpuScore))
-                            .font(.largeTitle)
-                    }
-                    Spacer()
-                }
-                .foregroundStyle(.white)
-                Spacer()
-            }
+       
         }
         
     }
     
-    func deal() {
-        let playerCardValue = Int.random(in: 2...14)
-        playerCard = "card" + String(playerCardValue)
-        
-        let cpuCardValue = Int.random(in: 2...14)
-        cpuCard = "card" + String(cpuCardValue)
-        
-        if playerCardValue > cpuCardValue {
-            playerScore += 1
-            
-        }
-        else if cpuCardValue > playerCardValue {
-            
-            cpuScore += 1
-        }
-        else {
-            playerScore -= 1
-            cpuScore -= 1
-        }
-    }
-}
-
+   
+    
 #Preview {
     ContentView()
 }
