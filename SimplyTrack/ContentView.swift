fe4f10bc167f9d8d400ebfc1582db2d036c1d12f
Farhanhamim1111 <farhanhamim1111@gmail.com>
2025-07-13 19:54:35 -0400

    spacing

--- SimplyTrack/ContentView.swift
+++ SimplyTrack/ContentView.swift
@@ -11,68 +11,56 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack{
-            HStack{
-                Spacer()
+        VStack (spacing: 10){
+            HStack (spacing:75){
+               
                 
                 Button{
                     
                 } label: {
-//
+
                     Image(systemName: "gearshape")
                         
                 }
                     .foregroundStyle(.blue)
-                    .font(Font.system(size:40))
+                    .font(Font.system(size:27))
                     .buttonStyle(.bordered)
                     
-                    
-                
-                
-                Spacer()
                 Image("logo")
                     .resizable()
                     .frame(width:100, height: 80)
-                Spacer()
+              
                 
-                //
                 Button{
                     
                 } label: {
-//
                     Image(systemName: "plus")
                 }
                 .buttonStyle(.bordered)
                 
                 .foregroundStyle(.blue)
-                .font(Font.system(size:40))
-                
-                
-                Spacer()
+                .font(Font.system(size:27))
                 
             }
             ZStack{
                 
-                RoundedRectangle(cornerRadius: 10)
+                RoundedRectangle(cornerRadius: 26)
                     .foregroundStyle(.white)
                     .shadow(radius: 10.0)
+                    .frame(width:360, height:90)
                 
                 Text("Current Activity: ")
                     .font(.system(size:25, weight: .medium, design: .default))
-                    
-                
             }
             
-            
-            
-            Spacer()
-            
-            RoundedRectangle(cornerRadius: 13)
+        
+            RoundedRectangle(cornerRadius: 26)
                 .foregroundStyle(.white)
-                .frame(width:340, height: 560)
+                .frame(width:360, height: 500)
                 .shadow(radius: 10.0)
-                .padding()
+                .aspectRatio(contentMode: .fit)
         }
+        Spacer()
         
         
         
